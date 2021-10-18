7c1232c66b7accccaf6461ac4d9a070f7ef923c8
조용일 <yedn1128@naver.com>
2021-10-18 10:15:14 +0900

    test clone

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ class App extends Component {
       <div>
         <h1>The number is : {this.state.count}</h1>
         <button onClick={this.add}>Add</button>
-        <button onClick={this.minus}>Minus</button>
+        <button onClick={this.minus}>MinuS</button>
       </div>
     );
   }
